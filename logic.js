7229cbc2d926d7c638ba8789c12ca6bc93f38112
KiritoGames20 <spam1006@web.de>
2021-02-03 16:34:55 +0100

    DogeCoin Init

---
+++ logic.js
@@ -0,0 +1,10 @@
+< script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" > < /script>
+
+<
+script >
+    $.get("https://chain.so/api/v2/get_info/DOGE", function(response) {}); <
+/script>
+
+
+GET / api / v2 / get_price / { DOGE }
+/{EU}
